df49f2e6265419380facc4c6c6f2132efaa58471
Fedir-Husar <105937067+Fedir-Husar@users.noreply.github.com>
2023-02-05 09:11:36 +0200

    Task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+const ingradientsEl = [];
+console.log(list);
+ingredients.forEach( element =>{
+const ingradEl = document.createElement('li');
+ingradEl.textContent = element;
+ingradEl.classList.add('item');
+ingradientsEl.push(ingradEl);
+});
+list.append(...ingradientsEl);
+
