0fdf950134ddbe8b898214dc41e82ac1f46884b3
Fedir-Husar <105937067+Fedir-Husar@users.noreply.github.com>
2023-02-06 18:15:43 +0200

    Update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const button = document.querySelector('.change-color');
+
+
+
+const handleClick = event => {
+
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  button.innerHTML = getRandomHexColor();
+};
+
+button.addEventListener("click", handleClick);
