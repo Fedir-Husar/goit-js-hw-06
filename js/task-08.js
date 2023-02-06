1ac938d00eb7844cb94df0e8d520cb0baa126d1a
Fedir-Husar <105937067+Fedir-Husar@users.noreply.github.com>
2023-02-06 16:54:44 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", submitHandler);
+
+
+
+function submitHandler(event) {
+  event.preventDefault();
+  const {
+      elements: { email, password } } = event.currentTarget;
+    if (email.value == "" || password.value == "") {
+        return alert("Error, all fields must be filled!");
+    } 
+    else {
+         console.log(`Email: ${email.value}, Password: ${password.value}`);
+         event.currentTarget.reset();
+    }
+}
